8ab84186dd5976911bad15e9e4e962d92b18dfa5
cao <2050997163@qq.com>
2022-08-15 17:22:22 +0800

    add temp.js

---
+++ temp.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         hello world
+// @namespace    http://www.frienda.com
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("hello world")
+})();
