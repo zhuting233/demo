8a72db908bcfe48d9a6a2dd40f7c0d23998e7557
cao <2050997163@qq.com>
2022-08-15 17:51:11 +0800

    change match

--- temp.js
+++ temp.js
@@ -2,14 +2,14 @@
 // @name         hello world
 // @namespace    http://www.frienda.com
 // @version      0.1
-// @description  try to take over the world!
+// @description  hello!
 // @author       cao
-// @license MIT
-// @match        http://*/*
+// @license      MIT
+// @match        https://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
-
+ 
 (function() {
     'use strict';
     console.log("hello world")
